4648dbb7d4d826246acad658a55af96b4b2f205e
Ian Christiansen <ianc.songdog@gmail.com>
2021-12-15 17:11:27 -0800

    added link path on booklist

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import { BrowserRouter, Link, Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
 import BookList from './views/Books/BookList';
+import BookDetail from './views/Books/BookDetail';
 import './App.css';
 
 function App() {
-  // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
       <BrowserRouter>
         <Link to="/books">Books</Link>
-
         <Switch>
-          <Route path="/books">
-            <BookList />
-          </Route>
+          <Route path="/books/:id" component={BookDetail} />
+          <Route path="/books" component={BookList} />
         </Switch>
       </BrowserRouter>
     </main>
